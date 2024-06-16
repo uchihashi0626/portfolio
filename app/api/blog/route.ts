ee9b8d0c248c78ffeb2994e001d8420770acc80b
Ryoto <70270271+uchihashi0626@users.noreply.github.com>
2024-06-17 04:00:55 +0900

    update

--- app/api/blog/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import blogData from "@/blog-data.json";
-
-const GET = () => {
-  return NextResponse.json(blogData);
-};
-
-export { GET };
--- app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <div className="container mx-auto py-[50px]">
-      <h2 className="text-[50px] text-center">404</h2>
-    </div>
-  );
-};
-
-export default NotFound;
